f35adeb21209d7855137151a629b999b090b1806
Ranika Williams <rnkwilliams07@gmail.com>
2024-05-10 14:27:36 -0500

    create rds

--- variables.tf
+++ variables.tf
@@ -55,3 +55,24 @@ variable "ssh_location" {
   description = "ip address that can ssh into the servers"
   type        = string
 }
+
+# rds variables
+variable "database_snapshot_identifier" {
+  description = "database snapshot name"
+  type        = string
+}
+
+variable "database_instance_class" {
+  description = "database instance type"
+  type        = string
+}
+
+variable "database_instance_identifier" {
+  description = "database instance identifier"
+  type        = string
+}
+
+variable "multi_az_deployment" {
+  description = "create a standby db instance"
+  type        = bool
+}
