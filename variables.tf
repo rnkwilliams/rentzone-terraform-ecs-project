3bf58865561b01db9b14ba3c1644bb081bb8d991
Ranika Williams <rnkwilliams07@gmail.com>
2024-05-10 14:06:21 -0500

    create security-group

--- variables.tf
+++ variables.tf
@@ -49,3 +49,9 @@ variable "private_data_subnet_az2_cidr" {
   description = "private data subnet az2 cidr block"
   type        = string
 }
+
+# security group variables
+variable "ssh_location" {
+  description = "ip address that can ssh into the servers"
+  type        = string
+}
