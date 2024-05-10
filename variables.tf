68a05744fd6ec179d29501c61a8d8372d43b98ee
Ranika Williams <rnkwilliams07@gmail.com>
2024-05-10 15:51:38 -0500

    create acm

--- variables.tf
+++ variables.tf
@@ -76,3 +76,14 @@ variable "multi_az_deployment" {
   description = "create a standby db instance"
   type        = bool
 }
+
+# acm variables
+variable "domain_name" {
+  description = "domain name"
+  type        = string
+}
+
+variable "alternative_names" {
+  description = "sub domain name"
+  type        = string
+}
