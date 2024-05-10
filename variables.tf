3a56e90d857e3950de239ea2d7558ef0a04fbdd1
Ranika Williams <rnkwilliams07@gmail.com>
2024-05-10 12:59:15 -0500

    create vpc

--- variables.tf
+++ variables.tf
@@ -13,3 +13,39 @@ variable "environment" {
   description = "environment to create resources"
   type        = string
 }
+
+# vpc variables
+variable "vpc_cidr" {
+  description = "vpc cidr block"
+  type        = string
+}
+
+variable "public_subnet_az1_cidr" {
+  description = "public subnet az1 cidr block"
+  type        = string
+}
+
+variable "public_subnet_az2_cidr" {
+  description = "public subnet az2 cidr block"
+  type        = string
+}
+
+variable "private_app_subnet_az1_cidr" {
+  description = "private subnet az1 cidr block"
+  type        = string
+}
+
+variable "private_app_subnet_az2_cidr" {
+  description = "private subnet az2 cidr block"
+  type        = string
+}
+
+variable "private_data_subnet_az1_cidr" {
+  description = "private data subnet az1 cidr block"
+  type        = string
+}
+
+variable "private_data_subnet_az2_cidr" {
+  description = "private data subnet az2 cidr block"
+  type        = string
+}
