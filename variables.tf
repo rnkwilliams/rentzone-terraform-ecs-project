2f33286439a3d044b6053f9d57154f2d1a02a280
Ranika Williams <rnkwilliams07@gmail.com>
2024-05-10 17:00:12 -0500

    create s3

--- variables.tf
+++ variables.tf
@@ -87,3 +87,14 @@ variable "alternative_names" {
   description = "sub domain name"
   type        = string
 }
+
+# s3 variables
+variable "env_file_bucket_name" {
+  description = "s3 bucket name"
+  type        = string
+}
+
+variable "env_file_name" {
+  description = "env file name"
+  type        = string
+}
